8409c9fccac3e3a7318b4de6e0d850a7e699d908
harreh08 <61203061+harreh08@users.noreply.github.com>
2021-03-30 22:54:03 +0700

    Add files via upload

---
+++ main/java/com/example/praktpm1/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.praktpm1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void pindah(View view) {
+        Intent intent = new Intent( MainActivity.this, second_activity.class);
+        startActivity(intent);
+    }
+}
